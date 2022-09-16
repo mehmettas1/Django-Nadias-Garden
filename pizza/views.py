f3fa2d915344184e7e7e676bf5891041c4209ec1
mehmet <tas349873@gmail.com>
2022-09-16 21:54:44 +0300

    update

--- pizza/views.py
+++ pizza/views.py
@@ -1,16 +1,26 @@
-from email.mime import multipart
 from django.shortcuts import render
-from pizza.forms import MultiplePizzaForm,PizzaForm
+from django.contrib import messages
+
+from pizza.forms import MultiplePizzaForm, PizzaForm
 
 # Create your views here.
 def home(request):
-    return render(request,"pizza/home.html")
+    return render(request, 'pizza/home.html')
 
 def order(request):
     multiple_form = MultiplePizzaForm()
-    if request.method=="POST":
-        filled_form =PizzaForm(request.POST)
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
         if filled_form.is_valid():
-            created_pizza=filled_form.save()
-            created_pizza_pk=created_pizza.id
-            
+            created_pizza = filled_form.save()
+            created_pizza_pk = created_pizza.id
+
+            size = filled_form.cleaned_data.get('size')
+            topping1 = filled_form.cleaned_data.get('topping1')
+            topping2 = filled_form.cleaned_data.get('topping2')
+
+            messages.success(request, f'Thanks for ordering! Your {size}, {topping1} and {topping2} pizza is on its way!')
+
+            filled_form = PizzaForm()
+        else:
+            created_pizza_pk=
