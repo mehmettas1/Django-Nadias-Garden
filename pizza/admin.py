b927e503113507461a6f104262164908d940cd21
mehmet <tas349873@gmail.com>
2022-09-16 21:07:05 +0300

    models

--- pizza/admin.py
+++ pizza/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Size,Pizza
 # Register your models here.
+admin.site.register(Size)
+admin.site.register(Pizza)
--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Size(models.Model):
+    title = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
+    
+class Pizza(models.Model):
+    topping1 = models.CharField(max_length=100)
+    topping2 = models.CharField(max_length=100)
+    size = models.ForeignKey(Size,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.size},{self.topping1},{self.topping2}"
