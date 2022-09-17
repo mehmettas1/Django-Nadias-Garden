c5ed42cfe7f03f4596db928854a8280e2461efbf
mehmet <tas349873@gmail.com>
2022-09-17 15:32:25 +0300

    update

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,12 +1,12 @@
-from  django import forms
-from .models import Pizza,Size
-
+from dataclasses import fields
+from django import forms
+from .models import Pizza, Size
 
 class PizzaForm(forms.ModelForm):
     class Meta:
-        model=Pizza
-        fields = ["topping1","topping2","size"]
-        labels = {"topping1":"topping1","topping2":"topping2"}
-        
-class MultiPizzaForm(forms.Form):
-    number=forms.IntegerField(min_value=2,max_value=6)
+        model = Pizza
+        fields = ["topping1", "topping2", "size"]
+        labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value=2, max_value=6)
